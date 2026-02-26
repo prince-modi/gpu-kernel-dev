511a5b715eeed28d9c5073239bdd5496ce898ae8
rishabhranjan2 <rchittaranjan@airmettle.com>
2026-02-26 08:36:17 -0800

    virtual environment support to work with same dependencies

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
-pip install torch
-pip install triton
-pip install helion
+if python -c 'import sys; sys.exit(sys.version_info < (3, 10))'; then
+    echo "Python version is sufficient (>= 3.10)."
+else
+    echo "Error: Python version is less than 3.10."
+    exit 1
+fi
+
+pip install virtualenv
+
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
