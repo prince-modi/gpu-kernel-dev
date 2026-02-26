3e29c075fc7a9819c405b51075009617bbd2bc7c
rishabhranjan2 <rchittaranjan@airmettle.com>
2026-02-26 13:10:00 -0800

    Reactivation of helion tests for rms

--- bench-driver.py
+++ bench-driver.py
@@ -4,6 +4,7 @@ import torch_kernels.benchmark as torlb
 import triton
 import torch
 import os
+import random
 
 if torch.cuda.is_available():
     # Use GPU
@@ -22,8 +23,8 @@ else:
         x_names=['N'],  # argument names to use as an x-axis for the plot
         x_vals=[128 * i for i in range(2, 100)],  # different possible values for `x_name`
         line_arg='provider',  # argument name whose value corresponds to a different line in the plot
-        line_vals=['torch-rmsnorm','triton-rmsnorm_with_loops'],  #,'helion-helion_rms_kernel' possible values for `line_arg``
-        line_names=["Torch", "Triton w Loops"],  #,"Helion" label name for the lines
+        line_vals=['torch-rmsnorm','triton-rmsnorm_with_loops','helion-helion_rms_kernel'],  #,'helion-helion_rms_kernel' possible values for `line_arg``
+        line_names=["Torch", "Triton w Loops","Helion"],  #,"Helion" label name for the lines
         styles=[('blue', '-'), ('green', '-'), ('red', '-')],  # line styles
         ylabel="GB/s",  # label name for the y-axis
         plot_name="RMS-Norm-With-Varying-N",  # name for the plot. Used also as a file name for saving the plot.
@@ -33,8 +34,8 @@ else:
         x_names=['M'],  # argument names to use as an x-axis for the plot
         x_vals=[128 * i for i in range(2, 100)],  # different possible values for `x_name`
         line_arg='provider',  # argument name whose value corresponds to a different line in the plot
-        line_vals=['torch-rmsnorm','triton-rmsnorm_with_loops'],  # 'helion-helion_rms_kernel' possible values for `line_arg``
-        line_names=["Torch", "Triton w Loops"],  #,"Helion" label name for the lines
+        line_vals=['torch-rmsnorm','triton-rmsnorm_with_loops','helion-helion_rms_kernel'],  # 'helion-helion_rms_kernel' possible values for `line_arg``
+        line_names=["Torch", "Triton w Loops","Helion"],  #,"Helion" label name for the lines
         styles=[('blue', '-'), ('green', '-'), ('red', '-')],  # line styles
         ylabel="GB/s",  # label name for the y-axis
         plot_name="RMS-Norm-With-Varying-M",  # name for the plot. Used also as a file name for saving the plot.
@@ -43,7 +44,7 @@ else:
 def rms_benchmark(M: int, N: int, provider: str):
     x = torch.randn(M, N, device=DEVICE, dtype=torch.float32)
     w = torch.randn(N, device=DEVICE, dtype=torch.float32)
-    eps = torch.randn(1,1,device = DEVICE, dtype= torch.float32)
+    eps = random.random()
     split_name = provider.split('-')
     dsl_type = split_name[0]
     bench_name = split_name[1]
