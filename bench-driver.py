569572d8368dacf4cded2a628919e45a4bcb75f2
rishabhranjan2 <rchittaranjan@airmettle.com>
2026-02-27 14:10:59 -0800

    some changes

--- bench-driver.py
+++ bench-driver.py
@@ -71,23 +71,23 @@ if __name__ == "__main__":
     else:
         parser = argparse.ArgumentParser()
         group = parser.add_mutually_exclusive_group()
-        group.add_argument("--generate-kernel-dump", help="Specific kernel name to generate dump for (write as <bench-name>-<kernel-name>)",nargs='?',type=str)
+        group.add_argument("--generate-kernel-dump", help="Specific kernel name to generate dump for (write as <bench-name>-<kernel-name>)",nargs=1,type=str)
         group.add_argument("--run-benchmark", help="Specify list of benchmarks to run - comma-separated list",nargs=1,type=str)
         parser.add_argument("--M",help="Specify M dimensions - write multiple if there are multiple tensors(e.g., rmsnorm only has 1 M while attention has 3 Ms)",nargs='*',type=int)
         parser.add_argument("--N",help="Specify N dimensions - write multiple if there are multiple tensors(e.g., rmsnorm only has 1 N while attention has 3 Ns)",nargs='*',type=int)
         args = parser.parse_args()
 
         if args.generate_kernel_dump is not None:
-            kernel_name_string = args.generate_kernel_dump
+            kernel_name_string = args.generate_kernel_dump[0]
             split_string = kernel_name_string.split('-',maxsplit = 1)
             benchmark_name = split_string[0].lower()
             kernel_test_name = split_string[1].lower()
             if benchmark_name == 'rms_bench':
-                if len(args.M) != 1 or len(args.N) != 1:
-                    print(f'Require 1 M and N value, not len(M) = {len(args.M)} and len(N) = {len(args.N)}')
-                    sys.exit(1)
-                rms_benchmark(args.M[0],args.N[0],kernel_test_name)
-                print(f'Running {kernel_test_name} from {benchmark_name}; M = {args.M[0]} ; N= {args.N[0]}')
+                # if len(args.M) != 1 or len(args.N) != 1:
+                #     raise Exception(f'Require 1 M and N value, not len(M) = {len(args.M)} and len(N) = {len(args.N)}')
+                #for sake of completion, just hardcoding M, N; either manually adjust or find another way to automate (another python script instead of bash)
+                rms_benchmark(1024,1024,kernel_test_name)
+                # print(f'Running {kernel_test_name} from {benchmark_name}; M = {args.M[0]} ; N= {args.N[0]}')
             elif benchmark_name == 'flashattn_bench':
               #can be defined here
               pass
--- generate_dumps.sh
+++ generate_dumps.sh
@@ -1,3 +1,8 @@
+if [[ $(nsys --verion) -ne "0" || $(ncu --version) -ne "0" ]]; then
+    echo "Need nsys and/or ncu in order to run. Exiting..."
+    exit 1
+fi
+
 if (( $# < 2 )); then
     echo "Expected: bash generate_dumps.sh (nsys | ncu) <kernel>[ <kernel>]*"
     exit 1
@@ -14,7 +19,7 @@ kernels=${@:2}
 for kernel in $kernels; do
     #good for high-level kernels like pytorch and maybe even helion?
     if [ "$profiler_option" == "nsys" ]; then
-        nsys profile --trace=cuda,nvtx,osrt,cudnn -o $kernel-nsys.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
+        nsys profile --trace=cuda,nvtx,osrt,cudnn -o $kernel-nsys.rep python3 bench-driver.py --generate-kernel-dump=${kernel}
     else
         ncu -o $kernel-ncu.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
     fi
