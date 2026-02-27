e0b4333c4320249c78da3f2d4b60877578a6b702
rishabhranjan2 <rchittaranjan@airmettle.com>
2026-02-27 13:40:53 -0800

    adjustment to script

--- generate_dumps.sh
+++ generate_dumps.sh
@@ -1,12 +1,21 @@
 if (( $# < 2 )); then
-    echo "Expected: bash generate_dumps.sh <profiler-mode> <kernel>[ <kernel>]*"
+    echo "Expected: bash generate_dumps.sh (nsys | ncu) <kernel>[ <kernel>]*"
     exit 1
 fi
 profiler_option=$1
+
+if [[ "$profiler_option" != "nsys" && "$profiler_option" != "ncu" ]]; then
+    echo "Expected profiler option to be nsys or ncu. Received '$profiler_option' instead "
+    exit 1
+fi
+
 kernels=${@:2}
 
 for kernel in $kernels; do
     #good for high-level kernels like pytorch and maybe even helion?
-    nsys profile --trace=cuda,nvtx,osrt,cudnn -o $kernel-nsys.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
-    ncu -o $kernel-ncu.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
+    if [ "$profiler_option" == "nsys" ]; then
+        nsys profile --trace=cuda,nvtx,osrt,cudnn -o $kernel-nsys.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
+    else
+        ncu -o $kernel-ncu.rep python3 bench-driver.py --generate_kernel_dump=${kernel}
+    fi
 done
