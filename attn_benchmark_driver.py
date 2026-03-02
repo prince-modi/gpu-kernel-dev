c59faa935d396824ca2087b4bd03af7e78f1d882
Prince Modi <pmodi@ucsd.edu>
2026-03-01 19:09:35 -0800

    triton flash attention

--- attn_benchmark_driver.py
+++ attn_benchmark_driver.py
@@ -1,23 +1,29 @@
-import triton_kernels.benchmark as tlb
-import helion_kernels.benchmark as hlb
-import torch_kernels.benchmark as torlb
-import cutlass.cute as cute
-import cute_kernels.benchmark as curlb
-import triton
-import torch
 import os
 import random
 
-#based on https://github.com/triton-lang/triton/blob/main/python/tutorials/06-fused-attention.py
+import cutlass.cute as cute
+import torch
+import triton
+
+import cute_kernels.benchmark as curlb
+import helion_kernels.benchmark as hlb
+import torch_kernels.benchmark as torlb
+import triton_kernels.benchmark as tlb
+
+# based on https://github.com/triton-lang/triton/blob/main/python/tutorials/06-fused-attention.py
+
 
 def is_cuda():
     return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
 def is_hopper():
     return is_cuda() and torch.cuda.get_device_capability()[0] == 9
 
-#these are in ms
-warmup_count = 25 # this is anyways the default for triton, can adjust if feel it helps
-repetitions = 100 #default from triton - can increase if want - number of times kernel runs for measurements
+
+# these are in ms
+warmup_count = 25  # this is anyways the default for triton, can adjust if feel it helps
+repetitions = 100  # default from triton - can increase if want - number of times kernel runs for measurements
 
 if torch.cuda.is_available():
     # Use GPU
@@ -31,19 +37,25 @@ else:
     print("CUDA not available. Using CPU.")
     os.environ["TRITON_INTERPRET"] = "1"
 
-#assume causal is false
+# assume causal is false
 headdim_vals = [64, 128]
 BATCH, N_HEADS = 4, 32
 dtype = torch.float16
 # vary seq length for fixed head and batch=4
 
-line_vals=["torch-flash_attn", "helion-flashatt_fwd", "triton-forward","cute-flashatt_fwd"]
-line_names=["Torch", "Triton","Helion","Cute"]  #,"Helion" label name for the lines
-styles=[('blue', '-'), ('green', '-'), ('red', '-'),('yellow','-')]  # line styles
+
+line_vals = [
+    "torch-flash_attn",
+    "helion-flashatt_fwd",
+    "triton-forward",
+    "cute-flashatt_fwd",
+]
+line_names = ["Torch", "Helion", "Triton", "Cute"]
+styles = [("blue", "-"), ("green", "-"), ("red", "-"), ("yellow", "-")]
 if is_hopper():
-    line_vals.append('tk-tk_rms_norm')
-    line_names.append('Thunderkittens')
-    styles.append(('purple','-'))
+    line_vals.append("tk-tk_rms_norm")
+    line_names.append("Thunderkittens")
+    styles.append(("purple", "-"))
 
 
 flash_attn_bench_configs = []
@@ -58,47 +70,56 @@ for HEAD_DIM in headdim_vals:
                 x_vals=[2**i for i in range(10, 15)],
                 line_arg="provider",
                 ylabel="TFLOPS",
-                plot_name = f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-warp_specialize={warp_specialize}",
+                plot_name=f"fused-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-warp_specialize={warp_specialize}",
                 args={
                     "H": N_HEADS,
                     "BATCH": BATCH,
                     "HEAD_DIM": HEAD_DIM,
                     "warp_specialize": warp_specialize,
                 },
-            ))
+            )
+        )
 
 
 @triton.testing.perf_report(flash_attn_bench_configs)
-def attn_benchmark(N_CTX: int,H,BATCH: int,HEAD_DIM: int ,warp_specialize: bool, provider: str):
-    method = get_attn_benchmark(BATCH,H,N_CTX,HEAD_DIM,warp_specialize, provider)
-    ms = triton.testing.do_bench(lambda: method(), warmup = warmup_count, rep = repetitions)
-    #as per test_daoAI_lab file
-    tflops = lambda ms: (4.0 * BATCH * H * (N_CTX * N_CTX) * HEAD_DIM) / (ms * 1e-3) / 1e12
+def attn_benchmark(
+    N_CTX: int, H, BATCH: int, HEAD_DIM: int, warp_specialize: bool, provider: str
+):
+    method = get_attn_benchmark(BATCH, H, N_CTX, HEAD_DIM, warp_specialize, provider)
+    ms = triton.testing.do_bench(lambda: method(), warmup=warmup_count, rep=repetitions)
+    # as per test_daoAI_lab file
+    tflops = (
+        lambda ms: (4.0 * BATCH * H * (N_CTX * N_CTX) * HEAD_DIM) / (ms * 1e-3) / 1e12
+    )
     return tflops(ms)
 
-def get_attn_benchmark(BATCH: int,H: int,N_CTX: int,HEAD_DIM: int ,warp_specialize: bool, provider: str):
-    split_name = provider.split('-')
+
+def get_attn_benchmark(
+    BATCH: int, H: int, N_CTX: int, HEAD_DIM: int, warp_specialize: bool, provider: str
+):
+    split_name = provider.split("-")
     dsl_type = split_name[0]
     bench_name = split_name[1]
-    if dsl_type not in ['triton','helion','torch']:
-        qkv = torch.randn((BATCH,N_CTX,3,H,HEAD_DIM))
+    if dsl_type not in ["triton", "helion", "torch"]:
+        qkv = torch.randn((BATCH, N_CTX, 3, H, HEAD_DIM))
     else:
         q = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE)
         k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE)
         v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=DEVICE)
-    if dsl_type == 'triton':
-        return lambda: tlb.attn_benchmarks(bench_name,Q=q,K=k,V=v)
-    elif dsl_type == 'helion':
-        compiled_code = hlb.compile_attn_benchmark(bench_name,Q=q,K=k,V=v)
-        return lambda: hlb.attn_benchmarks(compiled_code,Q=q,K=k,V=v)
-    elif dsl_type == 'torch':
-        return lambda: torlb.attn_benchmarks(bench_name,Q=q,K=k,V=v)
-    elif dsl_type == 'cute':
-                                                                                #based on helion code - pls confirm
-        compiled_code = curlb.compile_fa2_benchmark(bench_name,qkv = qkv,softmax_scale = 1 / HEAD_DIM ** 0.5)
-        return lambda: compiled_code(qkv = qkv)
-    elif dsl_type == 'thunderkittens' and is_hopper():
-        raise Exception(f'Thunderkittens needs to be placed here!!!')
+    if dsl_type == "triton":
+        return lambda: tlb.attn_benchmarks(bench_name, Q=q, K=k, V=v)
+    elif dsl_type == "helion":
+        compiled_code = hlb.compile_attn_benchmark(bench_name, Q=q, K=k, V=v)
+        return lambda: hlb.attn_benchmarks(compiled_code, Q=q, K=k, V=v)
+    elif dsl_type == "torch":
+        return lambda: torlb.attn_benchmarks(bench_name, Q=q, K=k, V=v)
+    elif dsl_type == "cute":
+        # based on helion code - pls confirm
+        compiled_code = curlb.compile_fa2_benchmark(
+            bench_name, qkv=qkv, softmax_scale=1 / HEAD_DIM**0.5
+        )
+        return lambda: compiled_code(qkv=qkv)
+    elif dsl_type == "thunderkittens" and is_hopper():
+        raise Exception(f"Thunderkittens needs to be placed here!!!")
     else:
-        raise Exception(f'{provider} is not yet supported')
-
+        raise Exception(f"{provider} is not yet supported")
--- torch_kernels/benchmark.py
+++ torch_kernels/benchmark.py
@@ -1,37 +1,44 @@
 from torch_kernels.rmsnorm.torch_rmsnorm import rmsnorm_kernel_basic
 from torch_kernels.flash_attn.flash_attn_torch import sdpa_reference
 
+
 def check_args_rms(**kwargs):
-    if 'X' not in kwargs or 'w' not in kwargs or 'eps' not in kwargs:
-        raise Exception(f'Expected arguments (X,w,eps) are not in given arguments')
-    return (kwargs['X'],kwargs['w'],kwargs['eps'])
+    if "X" not in kwargs or "w" not in kwargs or "eps" not in kwargs:
+        raise Exception(f"Expected arguments (X,w,eps) are not in given arguments")
+    return (kwargs["X"], kwargs["w"], kwargs["eps"])
+
 
 def check_args_attn(**kwargs):
-    if 'Q' not in kwargs or 'K' not in kwargs or 'V' not in kwargs:
-        raise Exception(f'Expected arguments (Q,K,V) are not in given arguments')
-    return (kwargs['Q'],kwargs['K'],kwargs['V'])
+    if "Q" not in kwargs or "K" not in kwargs or "V" not in kwargs:
+        raise Exception(f"Expected arguments (Q,K,V) are not in given arguments")
+    return (kwargs["Q"], kwargs["K"], kwargs["V"])
 
 
-def rms_benchmarks(benchmark_name: str,**kwargs):
-    X,w,eps = check_args_rms(kwargs)    
-    if benchmark_name == 'rmsnorm':
-        rmsnorm_kernel_basic(X,w,eps)
+def rms_benchmarks(benchmark_name: str, **kwargs):
+    X, w, eps = check_args_rms(**kwargs)
+    if benchmark_name == "rmsnorm":
+        rmsnorm_kernel_basic(X, w, eps)
     else:
-        raise Exception(f'No kernel with name {benchmark_name}')
+        raise Exception(f"No kernel with name {benchmark_name}")
+
 
 def attn_benchmarks(benchmark_name: str, **kwargs):
-    q,k,v = check_args_attn(kwargs)
-    if benchmark_name == 'sdpa_reference':
-        sdpa_reference(q,k,v,None)
+    q, k, v = check_args_attn(**kwargs)  # FIXED
+    if benchmark_name in [
+        "sdpa_reference",
+        "flash_attn",
+    ]:  # Catches driver's "torch-flash_attn"
+        sdpa_reference(q, k, v, None)
     else:
-        raise Exception(f'No kernel with name {benchmark_name}')
+        raise Exception(f"No kernel with name {benchmark_name}")
 
-def torch_provide_benchmark(benchmark_name: str,**kwargs):
-    if 'rms' in benchmark_name:
-        rms_benchmarks(benchmark_name,kwargs)
-    elif 'flashattn' in benchmark_name or 'attn' in benchmark_name:
-        attn_benchmarks(benchmark_name,kwargs)
-    elif 'load' in benchmark_name:
-        raise Exception(f'Received unsupported kernel {benchmark_name}')
+
+def torch_provide_benchmark(benchmark_name: str, **kwargs):
+    if "rms" in benchmark_name:
+        rms_benchmarks(benchmark_name, **kwargs)
+    elif "flashattn" in benchmark_name or "attn" in benchmark_name:
+        attn_benchmarks(benchmark_name, **kwargs)
+    elif "load" in benchmark_name:
+        raise Exception(f"Received unsupported kernel {benchmark_name}")
     else:
-        raise Exception(f'Received unsupported kernel {benchmark_name}')
+        raise Exception(f"Received unsupported kernel {benchmark_name}")
